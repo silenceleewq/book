4a423fdbb518d9b39fd938e7e48e7b33958674a7
lirenqiang <958246321@qq.com>
2019-09-25 10:04:37 +0800

    default commit message

--- gitbook.sh
+++ gitbook.sh
@@ -8,9 +8,9 @@ git commit -am "default commit message"
 
 git push
 
-cp -R _book/ $HOME/BookSource/ # 将 _book 下生成的内容都复制到一个指定的地方.
+cp -R _book/ $HOME/git_repository/book/ # 将 _book 下生成的内容都复制到一个指定的地方.
 
-cd  $HOME/BookSource/
+cd  $HOME/git_repository/book/
 
 git add --all
 
